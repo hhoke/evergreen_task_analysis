27062f51902b16025d5ec4e6c1b0fc4455a09cab
hhoke <harris.hoke@10gen.com>
2020-09-07 13:00:19 -0400

    system failures

--- get_tasks.js
+++ get_tasks.js
@@ -4,6 +4,7 @@ function problemChild(){
 return db.tasks.aggregate([
 	{$match: {finish_time: {$gt: ISODate("2020-08-27T20:00:00.000Z")}}},
 	{$match: {"details.status": "failed"}},
+	{$match: {"details.type": "system"}},
 	{$match: {version: {$regex: '^mms.*'}}},
 	{$match: {"details.desc": {$not: /.*shell.exec.*/}}},
         {$match: {"details.desc": {$not: /.*subprocess.exec.*/}}},
