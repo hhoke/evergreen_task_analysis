c0208a3a05c854817158bfd015a35c1e2961b638
hhoke <harris.hoke@10gen.com>
2020-09-07 10:34:00 -0400

    EVG-12813 get stats for causes of purples

--- get_tasks.js
+++ get_tasks.js
@@ -2,9 +2,12 @@ rs.slaveOk();
 db = db.getSiblingDB('mci');
 function problemChild(){
 return db.tasks.aggregate([
-{$match: {finish_time: {$gt: ISODate("2020-08-27T20:00:00.000Z")}}},
-{$match: {finish_time: {$lt: ISODate("2020-08-28T18:00:00.000Z")}}},
-{$project: {create_time:1, dispatch_time:1, scheduled_time:1, start_time:1, finish_time:1, priority:1, distro:1, depends_on:1, version:1}},
+	{$match: {finish_time: {$gt: ISODate("2020-08-27T20:00:00.000Z")}}},
+	{$match: {"details.status": "failed"}},
+	{$match: {version: {$regex: '^mms.*'}}},
+	{$match: {"details.desc": {$not: /.*shell.exec.*/}}},
+        {$match: {"details.desc": {$not: /.*subprocess.exec.*/}}},
+	{$project: {"details.desc": 1}},
 ])};
 //toArray makes this valid json for later loading into python
 var x = problemChild().toArray();
