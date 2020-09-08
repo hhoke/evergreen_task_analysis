0c3b38d9e2559a7547ca36f07b786e5bbb94c6fe
hhoke <harris.hoke@10gen.com>
2020-09-08 11:26:56 -0400

    summary stats to send to cloud on system_failures

--- get_tasks.js
+++ get_tasks.js
@@ -2,12 +2,11 @@ rs.slaveOk();
 db = db.getSiblingDB('mci');
 function problemChild(){
 return db.tasks.aggregate([
-	{$match: {finish_time: {$gt: ISODate("2020-08-27T20:00:00.000Z")}}},
+	{$match: {finish_time: {$gt: ISODate("2020-08-25T20:00:00.000Z")}}},
+	{$match: {finish_time: {$lt: ISODate("2020-09-08T20:00:00.000Z")}}},
 	{$match: {"details.status": "failed"}},
 	{$match: {"details.type": "system"}},
 	{$match: {version: {$regex: '^mms.*'}}},
-	{$match: {"details.desc": {$not: /.*shell.exec.*/}}},
-        {$match: {"details.desc": {$not: /.*subprocess.exec.*/}}},
 	{$project: {"details.desc": 1}},
 ])};
 //toArray makes this valid json for later loading into python
