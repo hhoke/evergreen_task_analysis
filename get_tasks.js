77aad38e9bbce5eac2d29dd132a5002b4cad9ef1
hhoke <harris.hoke@10gen.com>
2020-09-11 14:08:49 -0400

    get all tasks matching version 5d9b46ed2fbabe301b7435f7

--- get_tasks.js
+++ get_tasks.js
@@ -2,12 +2,7 @@ rs.slaveOk();
 db = db.getSiblingDB('mci');
 function problemChild(){
 return db.tasks.aggregate([
-	{$match: {finish_time: {$gt: ISODate("2020-08-25T20:00:00.000Z")}}},
-	{$match: {finish_time: {$lt: ISODate("2020-09-08T20:00:00.000Z")}}},
-	{$match: {"details.status": "failed"}},
-	{$match: {"details.type": "system"}},
-	{$match: {version: {$regex: '^mms.*'}}},
-	{$project: {"details.desc": 1}},
+	{$match: {"version": "5d9b46ed2fbabe301b7435f7"}},
 ])};
 //toArray makes this valid json for later loading into python
 var x = problemChild().toArray();
