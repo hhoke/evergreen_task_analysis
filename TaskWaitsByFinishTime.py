efa4ed067b5a4efbe1e047a698af091d085ee079
hhoke <harris.hoke@10gen.com>
2020-09-24 00:06:34 -0400

    bicolor plot showing task wait and task run

--- TaskWaitsByFinishTime.py
+++ TaskWaitsByFinishTime.py
@@ -4,10 +4,11 @@ plots task waits by finish time.
 '''
 import datetime
 import plotly.express as px                                                                                                     
+import pandas as pd
 import ETA
 import DependencyAnalysis
 
-OUT_HTML = './sept21rhel62small.html'
+OUT_HTML = './twocolorsept21rhel62small.html'
 IN_JSON = './sept21rhel62small.json'
 
 def generate_timeline(df, start='scheduled_time', end='finish_time'):
@@ -21,15 +22,22 @@ def generate_timeline(df, start='scheduled_time', end='finish_time'):
 
 def generate_twocolor_timeline(df, start='begin_wait', middle='start_time', end='finish_time', sortby='scheduled_time'):
 
+    df = df.sort_values(by=[sortby])
+    df_copy = df.copy()
     # create identical copy and introduce color field so you can still group on same y point
-    df_copy = df
-    df["color"] = 1
-    df_copy["color"] = 2    
+    df["color"] = 'Waiting'
+    df.rename(columns = {start:'start'}, inplace = True)
+    df.rename(columns = {middle:'end'}, inplace = True)
 
-    newdf = df_copy.append(df)
-    
-    df_sorted = newdf.sort_values(by=[sortby])
-    fig = px.timeline(df_sorted, x_start=start, x_end=end, y=sortby, color="color") 
+    df_copy["color"] = 'Running'   
+    df_copy.rename(columns = {middle:'start'}, inplace = True)
+    df_copy.rename(columns = {end:'end'}, inplace = True)
+
+    newdf = pd.concat([df, df_copy]).sort_index(kind='merge')
+   
+    df_sorted = newdf 
+    print(df_sorted)
+    fig = px.timeline(df_sorted, x_start='start', x_end='end', color="color") 
     fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up 
     fig.update_layout({
     'plot_bgcolor': 'rgba(0, 0, 0, 0)',
@@ -60,19 +68,20 @@ def main():
         task_data.calculate_task_unblocked_time(task)
 
     # have to do this as a second loop to avoid pollutint the unblock calculations
-    for task in task_data.get_tasks():
+    generator = task_data.get_tasks({'begin_wait':[],'start_time':[],'finish_time':[]})
+    for task in generator:
         # add eleven seconds to avoid plotly wierdness
         task['start_time'] += datetime.timedelta(0,11)
-        task['finish_time'] += datetime.timedelta(0,11)
+        task['finish_time'] += datetime.timedelta(0,22)
 
 
-    df = task_data.dataframe(task_data.get_tasks({'begin_wait':[]}))
-    df_sorted = df.sort_values(by=['scheduled_time'])
-    fig = generate_timeline(df_sorted, start='begin_wait', end='start_time')
+    generator = task_data.get_tasks({'begin_wait':[],'start_time':[],'finish_time':[]})
+    df = task_data.dataframe(generator)
+    fig = generate_twocolor_timeline(df)
     fig.update_layout(title='begin_wait to start_time, ranked by scheduled_time')
     fig.show()
-    fig.write_html(OUT_HTML)
-    print('figure saved at {}'.format(OUT_HTML))
+    #fig.write_html(OUT_HTML)
+    #print('figure saved at {}'.format(OUT_HTML))
 
 if __name__ == '__main__':
     main()
