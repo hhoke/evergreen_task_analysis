56daf1b403228bf622f437f3adf918533d292234
hhoke <harris.hoke@10gen.com>
2021-01-12 23:13:55 +0000

    histogram analysis of rhel62 with long-wait feedback enabled

--- get_tasks.js
+++ get_tasks.js
@@ -2,8 +2,8 @@ rs.slaveOk();
 db = db.getSiblingDB('mci');
 function problemChild(){
 return db.tasks.aggregate([
-{$match: {finish_time: {$gt: ISODate("2020-10-14T12:00:00.000Z")}}},
-{$match: {finish_time: {$lt: ISODate("2020-10-15T08:00:00.000Z")}}},
+{$match: {finish_time: {$gt: ISODate("2021-01-11T16:00:00.000Z")}}},
+{$match: {finish_time: {$lt: ISODate("2021-01-11T24:00:00.000Z")}}},
 {$project: {create_time:1, dispatch_time:1, scheduled_time:1, start_time:1, finish_time:1, priority:1, distro:1, depends_on:1, version:1, display_only:1, generated_by:1}},
 ])};
 //toArray makes this valid json for later loading into python
