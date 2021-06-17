38163a334b957d924c1b5931968fd56b5b4c4640
hhoke <harris.hoke@10gen.com>
2021-06-17 11:44:36 -0400

    event_log

--- delete_task_events.js
+++ delete_task_events.js
@@ -1,7 +1,7 @@
 // intended to use to delete events from an evergreen primary
 db = db.getSiblingDB('mci');
 function deleteStuff(){
-	var bulk = db.items.initializeUnorderedBulkOp();
+	var bulk = db.event_log.initializeUnorderedBulkOp();
 	bulk.find( {r_type:"TASK", ts:{$lte:ISODate("2014-02-27T21:23:26.295Z")}} ).remove();
 	bulk.execute();
 }
