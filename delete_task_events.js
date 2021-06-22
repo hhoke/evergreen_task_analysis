3e79dd6268aefd5b8416efd5a8a8a9ae04d623ac
hhoke <harris.hoke@10gen.com>
2021-06-22 15:45:47 -0400

    tested delete_task_events.js in prod shell

--- delete_task_events.js
+++ delete_task_events.js
@@ -1,15 +1,12 @@
 // intended to use to delete events from an evergreen primary
 db = db.getSiblingDB('mci');
-max_items_to_delete = 500000;
-beginning_of_time = ISODate("2014-01-27T21:23:26.295Z")
-function deleteStuff(delete_before_datetime){
-	num_items_to_delete = db.event_log.count( {r_type:"TASK", ts:{$lte:delete_before_datetime}} );
-	if (num_items_to_delete < max_items_to_delete) {
-		divisor = num_items_to_delete / max_items_to_delete;
-		time_since_beginning = delete_before_datetime - beginning_of_time;
-		deleteStuff(beginning_of_time + (time_since_beginning/divisor));
-	} else {
-		num_items_to_delete = db.event_log.count( {r_type:"TASK", ts:{$lte:delete_before_datetime}} );
-	}	
+max_items_to_delete = 7568;
+ttl_time = ISODate("2021-03-24T21:23:26.295Z")
+logs_to_delete = db.event_log.find( {r_type:"TASK", ts:{$lte:ttl_time}} ).limit(max_items_to_delete).toArray();
+while (logs_to_delete.length > 0) {
+	sleep(1000);
+	logs_ids_to_delete = logs_to_delete.map(log => log._id);
+	db.event_log.deleteMany({_id: {$in:logs_ids_to_delete}});
+	logs_to_delete = db.event_log.find( {r_type:"TASK", ts:{$lte:ttl_time}} ).limit(max_items_to_delete).toArray();
+	printjson(logs_to_delete[0]);
 }
-deleteStuff(ISODate("2014-02-27T21:23:26.295Z")});
